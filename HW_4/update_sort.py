ca25e39b1513cf0018aa984ada8c944ad1d4f013
smilecool2012 <smilecool2012@gmail.com>
2022-01-30 13:02:19 +0200

    HW_4_update

--- HW_4/update_sort.py
+++ HW_4/update_sort.py
@@ -7,22 +7,14 @@ from threading import RLock, Thread
 
 
 class ThreadMoveFiles(Thread):
-    def __init__(self, path, item_name, file_type, folder_name, main_path, locker):
+    def __init__(self, path, locker):
         super().__init__()
         self.path = path
-        self.item_name = item_name
-        self.file_type = file_type
-        self.folder_name = folder_name
-        self.main_path = main_path
         self.locker = locker
         
     def run(self):
         self.locker.acquire()
-        check_exist_path = os.path.exists(f'{self.main_path}{folder_sep}{self.folder_name}{folder_sep}{self.item_name}')
-        if self.file_type in files_type.get(self.folder_name) and not check_exist_path:
-            path_from = f'{self.path}{folder_sep}{self.item_name}'
-            to_path = f'{self.main_path}{folder_sep}{self.folder_name}{folder_sep}{self.item_name}'
-            shutil.move(path_from, to_path)
+        find_files_and_sort(self.path)
         self.locker.release()
 
 
@@ -34,21 +26,7 @@ class ThreadRemove(Thread):
     
     def run(self):
         self.locker.acquire()
-        shutil.rmtree(self.path)
-        self.locker.release()
-
-
-class ThreadMoveArch(Thread):
-    def __init__(self, path_from, path_to_unpack, locker):
-        super().__init__()
-        self.path_from = path_from
-        self.path_to_unpack = path_to_unpack
-        self.locker = locker
-    
-    def run(self):
-        self.locker.acquire()
-        shutil.unpack_archive(self.path_from, self.path_to_unpack)
-        os.remove(self.path_from)
+        remove_empty_folders(self.path)
         self.locker.release()
 
 
@@ -65,6 +43,14 @@ def normalize(name):
     return rx.sub('_', name.translate(map_cyr_to_latin))
 
 
+def move_files(path, item_name, file_type, folder_name, base_path_to):
+    check_exist_path = os.path.exists(f'{base_path_to}{folder_sep}{folder_name}{folder_sep}{item_name}')
+    if file_type in files_type.get(folder_name) and not check_exist_path:
+        path_from = f'{path}{folder_sep}{item_name}'
+        to_path = f'{base_path_to}{folder_sep}{folder_name}{folder_sep}{item_name}'
+        shutil.move(path_from, to_path)
+
+
 def make_folders():
     for x in range(0, len(folder_list)):
         if not os.path.exists(f'{_dir}{folder_sep}{folder_list[x]}'):
@@ -72,7 +58,6 @@ def make_folders():
 
 
 def find_files_and_sort(path):
-    lock = RLock()
     for item_name in os.listdir(path):
         path_from = f'{path}{folder_sep}{item_name}'
         if item_name not in folder_list and os.path.isfile(path_from) and item_name != '.DS_Store':
@@ -81,11 +66,10 @@ def find_files_and_sort(path):
             path_to_unpack = f'{main_path}{folder_sep}archives{folder_sep}{rem_suffix}'
             for folder_name in folder_list:
                 if folder_name != 'archives':
-                    move = ThreadMoveFiles(path, item_name, file_type, folder_name, main_path, lock)
-                    move.start()
+                    move_files(path, item_name, file_type, folder_name, main_path)
                 elif file_type in files_type.get('archives') and not os.path.exists(path_to_unpack):
-                    move_arch = ThreadMoveArch(path_from, path_to_unpack, lock)
-                    move_arch.start()
+                    shutil.unpack_archive(path_from, path_to_unpack)
+                    os.remove(path_from)
         elif item_name not in folder_list and os.path.isdir(path_from):
             find_files_and_sort(path_from)
 
@@ -105,15 +89,23 @@ def rename_files_and_folders(path):
 
 
 def remove_empty_folders(path_abs):
-    lock = RLock()
     walk = list(os.walk(path_abs))
     for path, folders, folders_items in walk[::-1]:
         folder_name = path.split(folder_sep)[-1]
         len_0 = len(os.listdir(path)) == 0 and folder_name not in folder_list
         len_1 = len(os.listdir(path)) == 1 and folder_name not in folder_list and '.DS_Store' in folders_items
         if len_0 or len_1:
-            remove = ThreadRemove(path, lock)
-            remove.start()
+            shutil.rmtree(path)
+
+
+def sort():
+    lock = RLock()
+    make_folders()
+    rename_files_and_folders(str(_dir))
+    move = ThreadMoveFiles(str(_dir), lock)
+    move.start()
+    remove_empty_folders = ThreadRemove(_dir, lock)
+    remove_empty_folders.start()
 
 
 if __name__ == '__main__':
@@ -131,7 +123,4 @@ if __name__ == '__main__':
         'archives': ['zip', 'gz', 'tar'],
         'documents': ['doc', 'docx', 'txt', 'pdf', 'xlsx', 'pptx'],
     }
-    make_folders()
-    rename_files_and_folders(str(_dir))
-    find_files_and_sort(str(_dir))
-    remove_empty_folders(_dir)
+    sort()
