c228c1a2157cdb98af87209cb1b29694103afca9
smilecool2012 <smilecool2012@gmail.com>
2022-05-09 19:51:04 +0300

    HW_13

--- HW_13 Django/myfinance/myfinance/settings.py
+++ HW_13 Django/myfinance/myfinance/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3!8wz7(w$rr_h1+cq240-ox0v^rolg119d4%#ka52g$bsm=k6*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
